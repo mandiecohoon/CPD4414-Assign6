15570128a7e7ff1790b40ebb931c83127b141382
Amanda Cohoon <mandiecohoon@gmail.com>
2015-04-03 12:19:02 -0400

    Initial commit

---
+++ src/java/database/Database.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package database;
+
+import java.io.StringWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.json.Json;
+import javax.json.stream.JsonGenerator;
+import javax.json.stream.JsonGeneratorFactory;
+import services.ProductsREST;
+
+/**
+ *
+ * @author Amanda Cohoon - c0628569
+ */
+public class Database {
+    public static Connection getConnection() throws SQLException {
+        Connection conn = null;
+        
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            String jdbc = "jdbc:mysql://localhost/CPD4414";
+            conn = DriverManager.getConnection(jdbc, "root", ""); 
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ProductsREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return conn;
+    }
+    
+    public static String getResults(String query, String... params) {
+        String result = "";
+        
+        try (Connection conn = getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            
+            ResultSet rs = pstmt.executeQuery();
+            StringWriter out = new StringWriter();
+            JsonGeneratorFactory factory = Json.createGeneratorFactory(null);
+            JsonGenerator gen = factory.createGenerator(out);
+            
+            gen.writeStartArray();
+            while (rs.next()) {
+                gen.writeStartObject()
+                    .write("productId", rs.getInt("productID"))
+                    .write("name", rs.getString("name"))
+                    .write("description", rs.getString("description"))
+                    .write("quantity", rs.getInt("quantity"))
+                    .writeEnd();
+            }
+            gen.writeEnd();
+            gen.close();
+            result = out.toString();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductsREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+    
+    public static int doUpdate(String query, String... params) {
+        int numChanges = 0;
+        try (Connection conn = getConnection()) {
+            PreparedStatement pstmt = conn.prepareStatement(query);
+            for (int i = 1; i <= params.length; i++) {
+                pstmt.setString(i, params[i - 1]);
+            }
+            numChanges = pstmt.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductsREST.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return numChanges;
+    }
+}
---
+++ src/java/services/ProductsREST.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package services;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.json.JsonObject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import model.Product;
+import model.ProductList;
+
+/**
+ *
+ * @author Amanda Cohoon - c0628569
+ */
+
+@Path("/products")
+@ApplicationScoped
+public class ProductsREST {
+    
+    @Inject
+    ProductList productList;
+    
+    @GET
+    @Produces("application/json")
+    public Response doGet() {
+        return Response.ok(productList.toJSON()).build();
+    }
+    
+    @GET
+    @Path("{productId}")
+    @Produces("application/json")
+    public Response doGet(@PathParam("productId") int id) {
+        return Response.ok(productList.get(id).toJSON()).build();
+    }
+    
+    @POST
+    @Consumes("application/json")
+    public Response doPost(JsonObject json) {
+        Response result;
+        
+        try {
+            productList.add(new Product(json));
+            result = Response.ok().build();
+        } catch (Exception e) {
+            result = Response.status(500).build();
+            System.out.println(e);
+        }
+        return result;
+    }
+   
+    @PUT
+    @Path("{id}")
+    @Consumes("application/json")
+    public Response doPut(@PathParam("id") int id, JsonObject json) {
+        Response result;
+        
+        try {
+            productList.set(id, new Product(json));
+            result = Response.ok().build();
+        } catch (Exception e) {
+            result = Response.status(500).build();
+        }
+        return result;
+    }
+    
+    @DELETE
+    @Path("{id}")
+    public Response doDelete(@PathParam("id") int id) {
+        Response result;
+        
+        try {
+            productList.remove(id);
+            result = Response.ok().build();
+        } catch (Exception e) {
+            result = Response.status(500).build();
+        }
+        return result;
+    }
+    
+}
